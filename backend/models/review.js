8c5afd4e723e35531584fa1e1d844d4c62471b8e
Hamshaverthini K <hamshaverthinik@gmail.com>
2025-05-17 09:40:47 +0530

    controllers done

--- backend/models/review.js
+++ backend/models/review.js
@@ -4,7 +4,7 @@ const reviewSchema = new mongoose.Schema({
   recipeId: { type: mongoose.Schema.Types.ObjectId, ref: "Recipe" },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   rating: { type: Number, min: 1, max: 5 },
-  comment: {String},
+  comment: {type:String},
 }, { timestamps: true });
 
 module.exports = mongoose.model("Review", reviewSchema);
