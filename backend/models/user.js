0712fd79482fd6378e9f331204e2805f8f3b70c4
Hamshaverthini K <hamshaverthinik@gmail.com>
2025-09-25 15:26:22 +0530

    dish done

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true},
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  confirmPassword:{type:String}
-
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
