15b08be81edf83006a3e37f5c5eabba130db9166
Hamshaverthini K <hamshaverthinik@gmail.com>
2026-09-07 10:13:57 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,10 @@ if (!fs.existsSync(uploadsDir)) {
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
